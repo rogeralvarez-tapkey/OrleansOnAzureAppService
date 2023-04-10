ae79d15ee3d2a19d500919fa9c9e59170a176cf0
Roger Alvarez <roger.alvarez@tapkey.com>
2023-04-10 06:35:13 -0500

    chore: serialization attributes Orleans

--- OrleansOnAppService.Abstractions/Visitor.cs
+++ OrleansOnAppService.Abstractions/Visitor.cs
@@ -1,11 +1,18 @@
 ﻿namespace OrleansOnAppService.Abstractions
 {
+    [GenerateSerializer]
     public class Visitor
     {
+
+        [Id(0)]
         public string SessionKey { get; set; } = string.Empty;
+        [Id(1)]
         public string RemoteAddress { get; set; } = string.Empty;
+        [Id(2)]
         public string CurrentPage { get; set; } = string.Empty;
+        [Id(3)]
         public DateTime Arrived { get; set; } = DateTime.Now;
+        [Id(4)]
         public DateTime LastSeen { get; set; } = DateTime.Now;
     }
 }
